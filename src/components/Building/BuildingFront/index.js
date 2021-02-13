9bc03ce1b30b56badbf3c21d0af4e22a7d667f79
felipefn <felipef.neves@hotmail.com>
2021-02-13 01:59:39 -0300

    feat: add building front

---
+++ src/components/Building/BuildingFront/index.js
@@ -0,0 +1,12 @@
+import StyledBuildingFront from "./styled";
+import BuildingBodyFront from "./BuildingBodyFront";
+
+function BuildingFront() {
+  return (
+    <StyledBuildingFront>
+      <BuildingBodyFront></BuildingBodyFront>
+    </StyledBuildingFront>
+  );
+}
+
+export default BuildingFront;
